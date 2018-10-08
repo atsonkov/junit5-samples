70a71f2851a1d7d46e5db166127cb9371625db1c
atsonkov <atsonkov@users.noreply.github.com>
2018-10-08 17:45:12 +0300

    Update CalculatorTests.java

--- junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
@@ -30,7 +30,7 @@ class CalculatorTests {
 	@CsvSource({
 			"0,    1,   1",
 			"1,    2,   3",
-			"49,  51, 100",
+			"49,  51, 99",
 			"1,  100, 101"
 	})
 	void add(int first, int second, int expectedResult) {
